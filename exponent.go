d99590f2e52c308d16308fddc62378482b5dee32
Daniel Hoelbling-Inzko <daniel.hoelbling-inzko@bitmovin.com>
2019-09-07 09:18:36 +0200

    Implementation of ExponentialBackOffs and success

---
+++ exponent.go
@@ -0,0 +1,57 @@
+package exponent
+
+import (
+	"math"
+	"math/rand"
+	"time"
+)
+
+type strategy func(e *exp) time.Duration
+
+var DecorrelatedJitter = func(e *exp) time.Duration {
+	ex := math.Exp2(float64(e.n))
+	jitter := rand.Int63n(int64(ex) / 2)
+	return time.Duration(jitter+int64(ex/2)) * time.Millisecond
+}
+var FullJitter = func(e *exp) time.Duration {
+	ex := math.Exp2(float64(e.n))
+	jitter := rand.Int63n(int64(ex))
+	return time.Duration(jitter) * time.Millisecond
+}
+var ExponentialBackoff = func(e *exp) time.Duration {
+	return time.Duration(math.Exp2(float64(e.n))) * time.Millisecond
+}
+var LinearBackoff = func(e *exp) time.Duration {
+	return time.Duration(e.n*100) * time.Millisecond
+}
+
+type exp struct {
+	n        int
+	retries  int
+	max      time.Duration
+	strategy strategy
+	done     bool
+}
+
+func (e *exp) Do() bool {
+	e.n++
+	return !e.done && e.n <= e.retries
+}
+
+func (e *exp) WaitFor() time.Duration {
+	return e.strategy(e)
+}
+
+func (e *exp) Wait() time.Duration {
+	sleep := e.WaitFor()
+	time.Sleep(sleep)
+	return sleep
+}
+
+func (e *exp) Success() {
+	e.done = true
+}
+
+func NewExponent(retries int, max time.Duration) *exp {
+	return &exp{retries: retries, max: max, strategy: DecorrelatedJitter}
+}
--- exponent_test.go
+++ exponent_test.go
@@ -7,6 +7,7 @@ import (
 
 func TestExponentialBackoff(t *testing.T) {
 	e := NewExponent(13, 10*time.Second)
+	e.strategy = LinearBackoff
 	var n int
 	t.Log("starting")
 	for e.Do() {
@@ -17,5 +18,64 @@ func TestExponentialBackoff(t *testing.T) {
 	if n != 13 {
 		t.Errorf("Expected 13 loops but only got %d", n)
 	}
+}
 
+func TestExponentialBackoffStopsOnSuccess(t *testing.T) {
+	e := NewExponent(10, 10*time.Second)
+
+	var n int
+	for e.Do() {
+		n++
+		if n == 4 {
+			e.Success()
+		}
+	}
+	if n != 4 {
+		t.Errorf("Expected to run only 4 times got %d", n)
+	}
+}
+
+func TestSuccess(t *testing.T) {
+	e := NewExponent(10, 10*time.Second)
+	e.Success()
+	if e.done != true {
+		t.Errorf("Expected done to be true got %v", e.done)
+	}
+}
+
+func BenchmarkWaitForFullJitter(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		e := NewExponent(20, 10*time.Second)
+		e.strategy = FullJitter
+		for e.Do() {
+			e.WaitFor()
+		}
+	}
+}
+func BenchmarkWaitForDecorrelatedJitter(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		e := NewExponent(20, 10*time.Second)
+		e.strategy = DecorrelatedJitter
+		for e.Do() {
+			e.WaitFor()
+		}
+	}
+}
+func BenchmarkWaitForExponentialBackoff(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		e := NewExponent(20, 10*time.Second)
+		e.strategy = ExponentialBackoff
+		for e.Do() {
+			e.WaitFor()
+		}
+	}
+}
+func BenchmarkWaitForLinearBackoff(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		e := NewExponent(20, 10*time.Second)
+		e.strategy = ExponentialBackoff
+		for e.Do() {
+			e.WaitFor()
+		}
+	}
 }
